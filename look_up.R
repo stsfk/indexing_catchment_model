4829e548da329b09e556dd4da51ce30607f7597d
stsfk <yyang90@connect.hku.hk>
2022-06-28 21:10:57 +0800

    working model

--- look_up.R
+++ look_up.R
@@ -9,7 +9,11 @@ pacman::p_load(tidyverse,
                hydroGOF,
                caret,
                tidymodels,
-               mlrMBO)
+               mlrMBO,
+               DiceKriging,
+               rgenoud,
+               GA,
+               ModelMetrics)
 
 # seed --------------------------------------------------------------------
 
@@ -57,61 +61,243 @@ data_process <- data_raw %>%
   rename(rating = NNSE) %>%
   mutate(record_id = 1:n())
 
-
-# keep 500 catchments
+# keep 500 catchments for model building and 30 catchments for look-up
 catchment_id_building <- sample(1:n_catchments, 500) %>% sort()
 catchment_id_lookup <- setdiff(1:n_catchments, catchment_id_building)
 
 data_process_building <- data_process %>%
-  filter(record_id %in% catchment_id_building)
+  filter(catchment_id %in% catchment_id_building)
 
-data_process_building <- data_process %>%
-  filter(record_id %in% catchment_id_lookup)
+data_process_lookup <- data_process %>%
+  filter(catchment_id %in% catchment_id_lookup)
 
 
-# Experiments -------------------------------------------------------------
+# Building model ----------------------------------------------------------
 
-train_portion = 0.6
+train_portion = 0.8
 val_portion = 0.2
-test_portion = 0.2
 
+data_train <- data_process_building %>%
+  group_by(model_id) %>%
+  sample_frac(train_portion) %>%
+  ungroup()
 
+data_val <- data_process_building %>%
+  filter(record_id %in% setdiff(data_process_building$record_id, data_train$record_id))
 
-prepare_modeling_data <-
-  function(frac = 0.1,
-           train_portion = 0.6,
-           val_portion = 0.2,
-           test_portion = 0.2) {
-    data_sample <- data_process %>%
-      group_by(model_id) %>%
-      sample_frac(frac) %>%
-      ungroup()
-    
-    data_train_val <- data_sample %>%
-      group_by(model_id) %>%
-      sample_frac(train_portion + val_portion) %>%
-      ungroup()
-    
-    data_train <- data_train_val %>%
-      group_by(model_id) %>%
-      sample_frac(train_portion / (val_portion + train_portion)) %>%
-      ungroup()
-    
-    data_val <- data_train_val %>%
-      filter(record_id %in% setdiff(data_train_val$record_id, data_train$record_id))
-    
-    data_test <- data_sample %>%
-      filter(record_id %in% setdiff(data_sample$record_id, data_train_val$record_id))
-    
-    list(
-      data_train = data_train %>% select(-record_id),
-      data_val = data_val %>% select(-record_id),
-      data_test = data_test %>% select(-record_id),
-      record_id_train = data_train$record_id,
-      record_id_val = data_val$record_id,
-      record_id_test = data_test$record_id
+record_id_train <- data_train$record_id
+record_id_val <- data_val$record_id
+
+train_set <- data_memory(
+  user_index = data_train$catchment_id,
+  item_index = data_train$model_id,
+  rating = data_train$rating,
+  index1 = T
+)
+
+val_set <- data_memory(
+  user_index = data_val$catchment_id,
+  item_index = data_val$model_id,
+  rating = data_val$rating,
+  index1 = T
+)
+
+train_val_set <- data_memory(
+  user_index = data_process_building$catchment_id,
+  item_index = data_process_building$model_id,
+  rating = data_process_building$rating,
+  index1 = T
+)
+
+scoringFunction <- function(x) {
+  dim <- x["dim"] %>% unlist()
+  lrate <- x["lrate"] %>% unlist()
+  niter <- x["niter"] %>% unlist()
+  
+  costp_l1 <- x["costp_l1"] %>% unlist()
+  costp_l2 <- x["costp_l2"] %>% unlist()
+  costq_l1 <- x["costq_l1"] %>% unlist()
+  costq_l2 <- x["costq_l2"] %>% unlist()
+  
+  r = Reco()
+  
+  r$train(
+    train_set,
+    opts = list(
+      dim = dim,
+      costp_l1 = costp_l1,
+      costp_l2 = costp_l2,
+      costq_l1 = costq_l1,
+      costq_l2 = costq_l2,
+      lrate = lrate,
+      niter = niter,
+      verbose = F,
+      nthread = nthread
     )
-  }
+  )
+  
+  rmse <-
+    ModelMetrics::rmse(actual = data_val$rating,
+                       predicted = r$predict(val_set))
+  
+  return(rmse)
+}
+
+obj_fun <- makeSingleObjectiveFunction(
+  fn = scoringFunction,
+  par.set = makeParamSet(
+    makeIntegerParam("dim", lower = 1, upper = 100),
+    makeNumericParam("lrate",  lower = 1e-04,   upper = 0.2),
+    makeIntegerParam("niter", lower = 5,  upper = 1000),
+    makeNumericParam("costp_l1",  lower = 0,   upper = 0.1),
+    makeNumericParam("costp_l2",  lower = 0,   upper = 0.1),
+    makeNumericParam("costq_l1",  lower = 0,   upper = 0.1),
+    makeNumericParam("costq_l2",  lower = 0,   upper = 0.1)
+  ),
+  has.simple.signature = FALSE,
+  minimize = TRUE
+)
+
+des <- generateDesign(
+  n = 5 * getNumberOfParameters(obj_fun),
+  par.set = getParamSet(obj_fun),
+  fun = lhs::randomLHS
+)
+
+des$y = apply(des, 1, obj_fun)
+
+control <- makeMBOControl() %>%
+  setMBOControlTermination(., iters = 40 - 5 * getNumberOfParameters(obj_fun))
+
+run <- mbo(
+  fun = obj_fun,
+  design = des,
+  control = control,
+  show.info = TRUE
+)
+
+r = Reco()
+
+opts <- run$x
+opts$nthread <- nthread
+opts$verbose <- F
+r$train(train_val_set, opts = opts)
+
+c(P,Q) %<-% r$output(out_memory(), out_memory())
+
+
+
+i <- 1
+j <- 4023
+
+retrieve_from_model <- function(i, j){
+  P[i,] %*% Q[j,] %>% as.vector()
+}
+n_evaluated <- 1000
+js <- sample(1:(n_model_instances*n_model_classes), n_evaluated)
+
+build_lm_data_set <- function(i, js){
+  
+  ratings <- data_process %>%
+    filter(catchment_id == i,
+           model_id %in% js) %>%
+    pull(rating)
+  
+  Q[js,] %>%
+    as.data.frame() %>%
+    mutate(rating = ratings) %>%
+    as_tibble()
+}
+
+data_lm <- build_lm_data_set(i, js)
+
+model <- lm(rating ~ . - 1, data = data_lm)
+preds <- predict(model, data_lm)
+
+plot(preds, data_lm$rating)
+
+
+
+js <- sample(1:(n_model_instances*n_model_classes), 100) %>% sort()
+ratings <- data_process %>%
+  filter(catchment_id == i,
+         model_id %in% js) %>%
+  pull(rating)
+
+js <- sort(js)
+
+i <- catchment_id_lookup[[2]]
+ratings <- data_process %>%
+  filter(catchment_id == i,
+         model_id %in% js) %>%
+  pull(rating)
+
+fn <- function(x){
+  
+  m1 <- matrix(x, nrow = 1)
+  m2 <- t(Q[js,])
+  
+  pred <- m1%*%m2 %>%
+    as.vector()
+  
+  -ModelMetrics::rmse(actual = ratings, predicted = pred)
+}
+
+GA <- ga(type = "real-valued", fitness = fn, lower = rep(-1, 43), upper = rep(1, 43), maxiter = 2000)
+
+
+pred <- GA@solution %*%
+  t(Q[js,]) %>%
+  as.vector()
+actual = ratings
+
+ModelMetrics::rmse(actual, pred)
+plot(actual, pred)
+
+
+js <- sample(1:(n_model_instances*n_model_classes), 1000) %>% sort()
+ratings <- data_process %>%
+  filter(catchment_id == i,
+         model_id %in% js) %>%
+  pull(rating)
+pred <- GA@solution %*%
+  t(Q[js,]) %>%
+  as.vector()
+actual = ratings
+
+ModelMetrics::rmse(actual, pred)
+cor(actual, pred)^2
+
+plot(actual, pred)
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+data_lm_all <- build_lm_data_set(i, 1:100)
+preds <- predict(model, data_lm_all)
+
+plot(preds, data_lm_all$rating)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 sparse_gof_wrapper <- function(frac = 0.1) {
